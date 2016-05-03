2f2563751539fd5b82236f523b7ea0938575c67b
Rich Hong <hong.rich@gmail.com>
2016-05-03 18:53:02 -0400

    Port over only show debug stack in DevMode from upstream (#5)
    https://github.com/revel/revel/pull/881

--- panic.go
+++ panic.go
@@ -21,12 +21,14 @@ func PanicFilter(c *Controller, fc []Filter) {
 // It cleans up the stack trace, logs it, and displays an error page.
 func handleInvocationPanic(c *Controller, err interface{}) {
 	error := NewErrorFromPanic(err)
-	if error == nil {
+	if error == nil && DevMode {
+		// Only show the sensitive information in the debug stack trace in development mode, not production
 		glog.Error(err, "\n", string(debug.Stack()))
 		c.Response.Out.WriteHeader(500)
 		c.Response.Out.Write(debug.Stack())
 		return
 	}
+
 	glog.Error(err, "\n", error.Stack)
 	c.Result = c.RenderError(error)
 }
